abcbb6a4feb011c7e112f400337d86128cd450df
Florent (Mac) <grenaille.florent85@gmail.com>
2023-05-08 20:16:43 +0200

    Add write json config function

--- helpers.py
+++ helpers.py
@@ -4,13 +4,6 @@ from datetime import datetime
 from datetime import timedelta
 
 
-ACCESS_TOKEN = {
-    'token': '',
-    'expire_at': datetime
-}
-REFRESH_TOKEN = ''
-
-
 def next_expire_time(expires_in: int) -> datetime:
     """Calculate the next expire time to dropbox API token.
 
@@ -51,3 +44,23 @@ def load_json_config(json_file_path: str) -> dict:
         data = json.load(json_file)
 
     return data
+
+
+def write_json_config(json_file_path: str, original_data: dict, new_data: dict):
+    """Write data in json file config.
+
+    Args:
+        json_file_path (str): json file object
+        original_data (dict): json file data in already present
+        new_data (dict): data to write in json file
+    """
+    serialize_data = {}
+    for k, v in new_data.items():
+        if isinstance(v, datetime):
+            serialize_data[k] = v.isoformat()
+        else:
+            serialize_data[k] = v
+
+    original_data.update(serialize_data)
+    with open(json_file_path, 'w') as json_file:
+        json.dump(original_data, json_file, indent=4, sort_keys=True)
