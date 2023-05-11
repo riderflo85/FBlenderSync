4b29d9ae14ee8bdbf521e90cabecd5ea476a8cfb
Florent (Mac) <grenaille.florent85@gmail.com>
2023-05-11 21:21:37 +0200

    Add the refresh token function and download dropbox file

--- server/dropbox.py
+++ server/dropbox.py
@@ -1,6 +1,10 @@
 import webbrowser
 import requests
 
+from helpers import load_json_config
+from helpers import next_expire_time
+from helpers import write_json_config
+from settings import CONFIG_FILE
 from settings import HOST
 from settings import PORT
 from .web import create_socket
@@ -54,12 +58,41 @@ class DropboxAPI:
         Returns:
             dict: headers for request
         """
+        kw_copy = kwargs.copy()
+        for k in kwargs.keys():
+            if '_' in k:
+                kw_copy[k.replace('_', '-')] = kw_copy.pop(k)
+
         headers = {
             'Authorization': f'Bearer {token}',
-            'Content-Type': 'application/json',
-            **kwargs
+            # 'Content-Type': 'application/json',
+            **kw_copy
         }
         return headers
+
+    def _is_expired_token(self, response: requests.Response) -> dict:
+        """Check if the DropBox response return a expired access token error.
+        If raise this error when get new access token with refresh token and
+        execute the callback function.
+
+        Args:
+            response (requests.Response): DropBox API response object with status code and body.
+
+        Returns:
+            dict: return fallback boolean with new token or with error message
+        """
+        if response.status_code == 401:
+            error = response.json()
+            error_accepted = ('expired_access_token', 'invalid_access_token')
+            if error['error']['.tag'] in error_accepted:
+                new_token = self.refresh_api_token()
+                return {'fallback': True, 'new_token': new_token}
+            else:
+                return {'fallback': False, 'error': str(error)}
+        elif response.status_code != 200:
+            return  {'fallback': False, 'error': response.text}
+        else:
+            return {'fallback': False}
 
     def get_access_token(self):
         """Get dropbox access token"""
@@ -86,25 +119,40 @@ class DropboxAPI:
         # "uid": "..."
         # }
 
-        if res.status_code != 200:
-            #TODO Renvoyer l'erreur dans l'interface de blender !!!
-            raise DropboxError(res.text)
-        else:
+        if res.status_code == 200:
             data = res.json()
             r_fields = ['access_token', 'refresh_token', 'expires_in']
             return {
                 k: v for k, v in data.items() if k in r_fields
             }
+        else:
+            #TODO Renvoyer l'erreur dans l'interface de blender !!!
+            raise DropboxError(res.text)
 
     def refresh_api_token(self):
         """Refresh the access token"""
+        config = load_json_config(CONFIG_FILE)
         url = f'{self.DRB_API}/oauth2/token'
         payload = {
             'grant_type': 'refresh_token',
-            'refresh_token': self.refresh_token,
+            'refresh_token': config['REFRESH_TOKEN'],
             'client_id': self.app_key,
             'client_secret': self.app_secret
         }
+        res = requests.post(url, data=payload)
+
+        if res.status_code == 200:
+            data = res.json()
+            end_validation = next_expire_time(data['expires_in'])
+            to_update_config = {
+                'ACCESS_TOKEN': data['access_token'],
+                'TOKEN_END_VALIDATION': end_validation
+            }
+            write_json_config(CONFIG_FILE, config, to_update_config)
+            return data['access_token']
+        else:
+            #TODO Renvoyer l'erreur dans l'interface de blender !!!
+            raise DropboxAPI(res.text)
 
     def get_content_folder(self, token: str, path: str) -> list:
         """Get the Dropbox content folder.
@@ -143,11 +191,33 @@ class DropboxAPI:
         #     "cursor": "WCOi340_Ghw3B5VCW5-MKZNs6LhaJ2vUDwj_ha_g",
         #     "has_more": false
         # }
+        result = self._is_expired_token(res)
+        if not result.get('error') and not result['fallback']:
+            res_data = res.json()
+            #TODO Voir pour gérer la pagination avec les valeurs `has_more` et `cursor`
+            return res_data['entries']
+        elif not result.get('error') and result['fallback']:
+            return self.get_content_folder(result['new_token'], path)
+        else:
+            #TODO Renvoyer l'erreur dans l'interface de blender !!!
+            raise DropboxError(result['error'])
 
+    def download_file(self, token: str, path: str) -> bytes:
+        """Download DropBox file.
+
+        Args:
+            token (str): User Access Token
+            path (str): Dropbox folder path
+
+        Returns:
+            bytes: File bytes content
+        """
+        url = f'{self.DRB_API}/2/files/download'
+        h = self._make_headers(token, Dropbox_API_Arg=f'{{"path": {path}}}')
+        res = requests.get(url, headers=h)
+        
         if res.status_code != 200:
             #TODO Renvoyer l'erreur dans l'interface de blender !!!
             raise DropboxError(res.text)
         else:
-            data = res.json()
-            #TODO Voir pour gérer la pagination avec les valeurs `has_more` et `cursor`
-            return data['entries']
+            return res.content
