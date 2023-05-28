8bf49d2566e3b6beeda046c311c16ebfae785e8d
Florent (Mac) <grenaille.florent85@gmail.com>
2023-05-28 16:23:04 +0200

    add the makearchive.sh script

---
+++ makearchive.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Create by Florent Grenaille
+# Create a .zip archive for install addon with Blender settings UI !
+
+FILES=(__init__.py preference.py mixin.py)
+# FOLDERS=(ui)
+# EXCLUDED_FILES=(ui/menu.py)
+
+SOURCE_CODE_PATH=code
+ARCHIVE_PATH=fblender_sync
+ARCHIVE_NAME=fblender_sync.zip
+
+
+function remove-archive {
+    rm -rf $ARCHIVE_PATH
+    rm $ARCHIVE_NAME
+}
+
+
+function create-archive {
+    mkdir $ARCHIVE_PATH
+
+    for file in "${FILES[@]}"
+    do
+        cp $SOURCE_CODE_PATH/$file $ARCHIVE_PATH
+    done
+
+    # for folder in "${FOLDERS[@]}"
+    # do
+    #     cp -r $SOURCE_CODE_PATH/$folder $ARCHIVE_PATH
+    # done
+
+    # for ex_file in "${EXCLUDED_FILES[@]}"
+    # do
+    #     rm $ARCHIVE_PATH/$ex_file
+    # done
+
+    zip -r "$ARCHIVE_NAME" "$ARCHIVE_PATH"
+    rm -rf $ARCHIVE_PATH
+}
+
+
+cd ..
+
+if test -f "$ARCHIVE_NAME"; then
+    remove-archive
+fi
+
+create-archive
