d5b2a9feb2942a6d48e30db0155735dd6b80cc01
Florent (Mac) <grenaille.florent85@gmail.com>
2024-07-25 17:12:20 +0200

    fix issue #3 - Bug when create root folder

--- operators/folder.py
+++ operators/folder.py
@@ -113,6 +113,21 @@ class NewFolderCloud(FDropBoxMixin, bpy.types.Operator):
     item_ui_list_index: IntProperty(name="Index of folder", default=-1)
     folder_name: StringProperty(name="Nom du dossier")
 
+    @staticmethod
+    def _add_folder(wm, data):
+        new_folder = wm.cloud_data.add()
+        new_available_folder = wm.available_folders.add()
+        new_folder.tag = "folder"
+        new_folder.is_folder = True
+        new_folder.name = new_available_folder.desc = data["name"]
+        new_folder.id = new_available_folder.id = data["id"]
+        new_folder.path_lower = data["path_lower"]
+        new_folder.path_display = new_available_folder.name = data["path_display"]
+        splited_path = data["path_lower"].split("/")
+        splited_path.pop(0)
+        new_folder.indent_level = len(splited_path) - 1
+        return new_folder
+
     @classmethod
     def poll(cls, context):
         return len(context.window_manager.cloud_data) > 0
@@ -128,30 +143,22 @@ class NewFolderCloud(FDropBoxMixin, bpy.types.Operator):
         bpy.context.window.cursor_set("WAIT") # Set the mouse cursor to WAIT icon
         cloud_response = self.cloud_action(context, "create_folder", path=root_path)
         bpy.context.window.cursor_set("DEFAULT")
-        if folder_parent.is_expanded:
-            new_folder = wm.cloud_data.add()
-            new_available_folder = wm.available_folders.add()
-            new_folder.tag = "folder"
-            new_folder.is_folder = True
-            new_folder.name = new_available_folder.desc = cloud_response["name"]
-            new_folder.id = new_available_folder.id = cloud_response["id"]
-            new_folder.path_lower = cloud_response["path_lower"]
-            new_folder.path_display = new_available_folder.name = cloud_response["path_display"]
-            splited_path = cloud_response["path_lower"].split("/")
-            splited_path.pop(0)
-            new_folder.indent_level = len(splited_path) - 1
-            if folder_parent:
+        if folder_parent:
+            new_folder = self._add_folder(wm, cloud_response)
+            if folder_parent.is_expanded:
                 self.move_ui_items(
                     moving_items=[new_folder],
                     insert_in=self.item_ui_list_index+1,
                     items=wm.cloud_data,
                     parent=folder_parent,
                 )
+            else:
+                app_ops = getattr(bpy.ops, APP_NAME)
+                folder_content_op_id = FolderContentOpMenu.bl_idname.split(".")[-1]
+                folder_content_operator = getattr(app_ops, folder_content_op_id)
+                folder_content_operator(item_ui_list_index=self.item_ui_list_index)
         else:
-            app_ops = getattr(bpy.ops, APP_NAME)
-            folder_content_op_id = FolderContentOpMenu.bl_idname.split(".")[-1]
-            folder_content_operator = getattr(app_ops, folder_content_op_id)
-            folder_content_operator(item_ui_list_index=self.item_ui_list_index)
+            self._add_folder(wm, cloud_response)
 
         self.folder_name = ""
         return {'FINISHED'}
